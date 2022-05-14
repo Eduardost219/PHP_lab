249908fa2a1f8fc542ea76100943875f4e62a872
Eduardost219 <eduard.ortega@boomlabs.edu.co>
2022-05-13 21:48:41 -0500

    configurando funcion detail

--- controllers/estudiante_controller.php
+++ controllers/estudiante_controller.php
@@ -24,11 +24,33 @@ class EstudianteController implements IController
 
             }
         }
+        conexionDB->close();
+        return $estudiantes;
     }
     public function detail($id)
     {
-
+        $sql = "SELECT * FROM estudiantes WHERE id =" .$id;
+        $conexionDB = new ConexionDB();
+        $resultQuery = $conexionDB->getResultQuery($sql);
+        $estudiante = null;
+        if($resultQuery->num_rows > 0){
+            while ($row = $resultQuery->fetch_assoc()){
+                $estudiante = new Estudiante();
+                $estudiante->set('id', $row['id']);
+                $estudiante->set('codigo', $row['codigo']);
+                $estudiante->set('nombres', $row['nombres']);
+                $estudiante->set('apellidos', $row['apellidos']);
+                $estudiante->set('edad', $row['edad']);
+            }
+        }
+        $conexionDB->close();
+        return $estudiante;
     }
+
+    public function create($estudianteModel){
+        
+    }
+
     public function update($id, $estudianteModel)
     {
 
