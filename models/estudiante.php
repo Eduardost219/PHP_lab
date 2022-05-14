8155caf08bf1272f2aefa7bc1474ad4853966c40
Eduardost219 <eduard.ortega@boomlabs.edu.co>
2022-05-13 20:36:47 -0500

    configuracion de la carpeta controller

---
+++ models/estudiante.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace models;
+
+class estudiantes_db{
+    protected $id;
+    protected $codigo;
+    protected $nombres;
+    protected $apellidos;
+    protected $edad;
+
+    public function get($nameField){
+        return $this->{$nameField};
+    }
+
+    public function set($nameField, $value){
+        $this->{$nameField} = $value;
+    }
+}
