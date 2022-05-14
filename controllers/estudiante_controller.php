ef521d246e617c7bb6c52907304b23beecc3c505
Eduardost219 <eduard.ortega@boomlabs.edu.co>
2022-05-13 21:02:47 -0500

    configurando funcion list

--- controllers/estudiante_controller.php
+++ controllers/estudiante_controller.php
@@ -8,7 +8,22 @@ use models\Estudiante;
 class EstudianteController implements IController
 {
     public function list(){
-        
+        $sql = "select * from estudiantes";
+        $conexionDB = new ConexionDB();
+        $resultQuery = $conexionDB->getResultQuery($sql);
+        $estudiantes = [];
+        if ($resultQuery->num_rows > 0){
+            while ($row = $resultQuery->fetch_assoc()){
+                $estudiante->set('id', $row['id']);
+                $estudiante->set('codigo', $row['codigo']);
+                $estudiante->set('nombres', $row['nombres']);
+                $estudiante->set('apellidos', $row['apellidos']);
+                $estudiante->set('edad', $row['edad']);
+
+                array.push($estudiantes, $estudiante);
+
+            }
+        }
     }
     public function detail($id)
     {
